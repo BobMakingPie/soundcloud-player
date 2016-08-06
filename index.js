798d700ba6efa17f5841d716e7037c05bc7c5e8b
Eddie Qi <eddieqi128@gmail.com>
2016-08-06 17:16:25 +1000

    Added autoply & made index.js easier to read'

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const {app, BrowserWindow} = require("electron");
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({frame: false, show: false, minWidth: 600, minHeight: 250});
+  win = new BrowserWindow({
+    frame: false,
+    show: false,
+    minWidth: 600,
+    minHeight: 250,
+    width: 1280,
+    height: 720
+  });
   win.loadURL(`file://${__dirname}/client/index.html`);
   win.toggleDevTools();
   win.on("closed", function () {
